45b7edd1b980f7b6891164962e209f18e59472b6
hagleyson <hagleyson@gmail.com>
2022-01-06 15:32:33 -0300

    secondary validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,36 +4,50 @@ const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+    if (enteredName.trim() !== "") {
+      setEnteredNameIsValid(true);
+      return;
+    }
   };
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
   const formSubmissionHandler = (e) => {
     e.preventDefault();
+    setEnteredNameTouched(true);
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
       return;
     }
     setEnteredNameIsValid(true);
+    // const enteredValue = nameInputRef.current.value;
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
+    setEnteredName("");
   };
-  // useEffect(() => {
-  //   nameInputRef.current.focus();
-  // }, []);
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className={`form-control ${!enteredNameIsValid && "invalid"}`}>
+      <div className={`form-control ${nameInputIsInvalid && "invalid"}`}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
           type="text"
           id="name"
           value={enteredName}
+          onBlur={nameInputBlurHandler}
           onChange={nameInputChangeHandler}
         />
       </div>
-      {!enteredNameIsValid && (
+      {nameInputIsInvalid && (
         <p className="error-text">Name must not be Empty</p>
       )}
       <div className="form-actions">
